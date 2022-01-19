bbf19fac3240fd0a4f58eb3cfaac40cd0c842e18
ToniaPsom <csd3878@csd.uoc.gr>
2022-01-19 22:23:50 +0200

    messages pt1

--- src/ui/src/Messages/MessageRows.js
+++ src/ui/src/Messages/MessageRows.js
@@ -2,13 +2,22 @@ import {
     Tr,
     Td,
     IconButton,
+    Button,
+    Grid,
+    GridItem,
+    Text,
+    HStack,
+    VStack,
+    Flex,
+    Avatar,
     useColorModeValue
 } from '@chakra-ui/react'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import $ from 'jquery';
 import { TiDelete } from "react-icons/ti";
+import dayjs from 'dayjs';
 
 
 
@@ -17,7 +26,10 @@ const MessageRows = (info) =>{
 
     const color = useColorModeValue('gray.100', 'gray.700')
     const deleteHover = useColorModeValue('red.500', 'red.400')
-    const [ isHovered, setIsHovered ] = useState(false);
+    const [ isLoaded, setIsLoaded ] = useState(false);
+    var relativeTime = require('dayjs/plugin/relativeTime')
+    dayjs.extend(relativeTime)
+    const betterTime = dayjs(info.info.date_time)
 
     const getAllMessages = () => {
 
@@ -33,13 +45,14 @@ const MessageRows = (info) =>{
                 contentType: 'application/json',
                 data: dets,
                 success: function (result) {
-                  console.log(info.info.sender);
-                  console.log(info.info.doctor_id);
-                  console.log("Success");
-                  var json = JSON.parse(result);
-                  console.log(result);
-                 // <Link to="/allMessages"></Link>
+                    console.log(info.info.sender);
+                    console.log(info.info.doctor_id);
+                    console.log("Success");
+                    var json = JSON.parse(result);
+                    console.log(result);
+                    // <Link to="/allMessages"></Link>
                     //setIsLoaded(true);
+                    
                 },
                 error: function (result) {
 
@@ -52,25 +65,46 @@ const MessageRows = (info) =>{
                 }
             });
         }
-    
-    
+
+
 
     return(
-        <Tr 
-            _hover = {{background: color}} 
-            onMouseEnter={() => {
-                setIsHovered(true)
-            }}
-            onMouseLeave={() => {
-                setIsHovered(false)
-            }}
-            h='65px'
+        <>
+
+            <Button 
+            h='75px'
+            w='100%'
+            background='gray.700'
+            _hover={{background: 'gray.700'}}
+            p={2}
             onClick={getAllMessages}
+
         >
-            <Td> { info.info.sender }</Td>
-            <Td> { info.info.message }</Td>
-            <Td> { info.info.date_time }</Td>
-        </Tr>
+            <Flex maxW='100%' alignItems='center'>
+                    <Avatar name={info.info.sender} size='sm' src='https://bit.ly/broken-link'  />
+
+
+                    <Grid templateRows='repeat(2, 1fr)' pl={2}>
+                        <GridItem rowSpan={1} isTruncated>
+                            <Flex w='100%' justifyContent='space-between' alignItems='baseline'>
+                                <Text isTruncated>{info.info.sender}</Text>
+
+                                <Text fontSize='xs' fontWeight='100'>{betterTime && betterTime.fromNow()}</Text>
+
+                            </Flex>
+                        </GridItem>
+
+                        <GridItem rowSpan={1} mt={1} isTruncated>
+                            <Text fontSize='sm' fontWeight='300' isTruncated >{info.info.sender + ": " + info.info.message}</Text>
+                        </GridItem>
+                    </Grid>
+                    
+
+            </Flex>
+        </Button>
+
+
+        </>
 
 
     )
--- src/ui/src/Messages/Messages.js
+++ src/ui/src/Messages/Messages.js
@@ -7,6 +7,13 @@ import {
     Th,
     Td,
     Skeleton,
+    Grid,
+    GridItem,
+    useColorModeValue,
+    Divider,
+    Box,
+    VStack,
+    Heading
 } from '@chakra-ui/react';
 import $ from 'jquery';
 import MessageRows from './MessageRows';
@@ -35,7 +42,7 @@ import { UserContext } from '../contexts/UserContext';
               console.log(userInfo.doctor_id);
               console.log("Success");
               var json = JSON.parse(result);
-              console.log(result);
+              console.log(json);
                 setMessages(json);
                 setIsLoaded(true);
             },
@@ -50,76 +57,33 @@ import { UserContext } from '../contexts/UserContext';
     ,
     []);
 
-
+    const leftColor = useColorModeValue('white', 'gray.700')
 
     return(
-        <Table variant='striped' colorScheme='grey'>
-          {isLoaded ?
-          <Tbody>
+        
+
+      <Box  w='90%' position='fixed' h='90%' mt={2} borderRadius='10px'>
+        <Grid background={leftColor} borderRadius='10px' templateColumns='repeat(10, 1fr)' h='100%'  w='100%' p={0}  >
+          <GridItem borderRight='1px' borderColor='gray.600' h='100%' colSpan={3} display='flex' justifyContent='center'>
+                        <VStack w='100%' mt={3}>
+                            <Heading fontSize='lg' >All messages</Heading>
+                            <Divider />
             
-            {messages.map(message => <MessageRows info={message} key={message.username} />)}
-          </Tbody>
-          :
-          <Tbody>
-            <Tr>
-            <Td><Skeleton height='40px' w='100%' /></Td>
-            <Td><Skeleton height='40px' w='100%' /></Td>
-            <Td><Skeleton height='40px' w='100%' /></Td>
-            <Td><Skeleton height='40px' w='100%' /></Td>
-            <Td><Skeleton height='40px' w='100%' /></Td>
-            </Tr>
-            <Tr>
-            <Td><Skeleton height='40px' w='100%' /></Td>
-            <Td><Skeleton height='40px' w='100%' /></Td>
-            <Td><Skeleton height='40px' w='100%' /></Td>
-            <Td><Skeleton height='40px' w='100%' /></Td>
-            <Td><Skeleton height='40px' w='100%' /></Td>
-            </Tr>
-            <Tr>
-            <Td><Skeleton height='40px' w='100%' /></Td>
-            <Td><Skeleton height='40px' w='100%' /></Td>
-            <Td><Skeleton height='40px' w='100%' /></Td>
-            <Td><Skeleton height='40px' w='100%' /></Td>
-            <Td><Skeleton height='40px' w='100%' /></Td>
-            </Tr>
-            <Tr>
-            <Td><Skeleton height='40px' w='100%' /></Td>
-            <Td><Skeleton height='40px' w='100%' /></Td>
-            <Td><Skeleton height='40px' w='100%' /></Td>
-            <Td><Skeleton height='40px' w='100%' /></Td>
-            <Td><Skeleton height='40px' w='100%' /></Td>
-            </Tr>
-            <Tr>
-            <Td><Skeleton height='40px' w='100%' /></Td>
-            <Td><Skeleton height='40px' w='100%' /></Td>
-            <Td><Skeleton height='40px' w='100%' /></Td>
-            <Td><Skeleton height='40px' w='100%' /></Td>
-            <Td><Skeleton height='40px' w='100%' /></Td>
-            </Tr>
-            <Tr>
-            <Td><Skeleton height='40px' w='100%' /></Td>
-            <Td><Skeleton height='40px' w='100%' /></Td>
-            <Td><Skeleton height='40px' w='100%' /></Td>
-            <Td><Skeleton height='40px' w='100%' /></Td>
-            <Td><Skeleton height='40px' w='100%' /></Td>
-            </Tr>
-            <Tr>
-            <Td><Skeleton height='40px' w='100%' /></Td>
-            <Td><Skeleton height='40px' w='100%' /></Td>
-            <Td><Skeleton height='40px' w='100%' /></Td>
-            <Td><Skeleton height='40px' w='100%' /></Td>
-            <Td><Skeleton height='40px' w='100%' /></Td>
-            </Tr>
-            <Tr>
-            <Td><Skeleton height='40px' w='100%' /></Td>
-            <Td><Skeleton height='40px' w='100%' /></Td>
-            <Td><Skeleton height='40px' w='100%' /></Td>
-            <Td><Skeleton height='40px' w='100%' /></Td>
-            <Td><Skeleton height='40px' w='100%' /></Td>
-            </Tr>
-          </Tbody>
-  }
-        </Table>
+            {messages.map(message => <MessageRows info={message} key={message.sender} />)}
+         
+                        
+                        </VStack>
+                    </GridItem>
+
+
+                    <GridItem  h='100%'  colSpan={7} display='flex' justifyContent='center'>
+                        <VStack>
+                    
+                        <Divider />
+                        </VStack>
+                    </GridItem>
+        </Grid>
+        </Box>
     )
   }
   export default Messages;
